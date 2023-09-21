7bbc0695aa27cc8c62fbf394047c29b7abced939
Murilo <mbalves1@outlook.com>
2023-09-21 14:36:04 -0300

    [fix]: conflict node diff

--- components/ListCards.vue
+++ components/ListCards.vue
@@ -1,30 +1,25 @@
 <template>
-  <div
-    class="card"
-  >
+  <div class="card">
     <v-list
       v-for="(card, cardx) in data"
       :key="cardx"
     >
       <div>
-        <Tilt>
-          <Card
-            class="mx-3 mr-1"
-            height="145"
-            width="100%"
-            min-width="250px"
-            max-width="250px"
-            elevation="3"
-            :card="card"
-            >
-          </Card>
-        </Tilt>
+        <Card
+          class="mx-3 mr-1"
+          height="145"
+          width="100%"
+          min-width="250px"
+          max-width="250px"
+          elevation="3"
+          :card="card"
+          >
+        </Card>
       </div>
     </v-list>
   </div>
 </template>
 <script setup>
-import Tilt from 'vanilla-tilt-vue'
 defineProps({
   data: {
     type: [Array, Object]
